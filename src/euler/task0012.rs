c039b7249021a89dcc821978c3e831162e8c84e3
Mikael Björk <bjorn.mikael.bjork@gmail.com>
2022-11-13 09:49:55 +0100

    Cleanup

--- src/euler/task0012.rs
+++ src/euler/task0012.rs
@@ -3,18 +3,6 @@ use crate::math::prime_sieve::PrimeSieve;
 
 use std::collections::HashMap;
 
-fn divisors(value: u64) -> usize {
-    let mut count = 0;
-
-    for divisor in 1..(1 + value) {
-        if value % divisor == 0 {
-            count += 1;
-        }
-    }
-
-    count
-}
-
 pub fn execute(input: &String) {
     let count: u64 = input.parse().unwrap();
 
@@ -40,8 +28,6 @@ pub fn execute(input: &String) {
         for factor_count in factor_counts {
             combination_count *= 1 + factor_count.1;
         }
-
-        println!("{} {}", number, combination_count);
 
         if combination_count > count {
             println!("{}", number);
